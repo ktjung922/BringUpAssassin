75562f631e6e18938ca03568f23fd78612a3a85a
ktjung922 <37677425+ktjung922@users.noreply.github.com>
2021-01-23 21:53:43 +0900

    1.3
    Phone

---
+++ Assassin/Assets/Script/ADmoManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+public class ADmoManager : MonoBehaviour
+{
+    public bool isTestMode;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBannerAd();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    AdRequest GetAdRequest() {
+        return new AdRequest.Builder().AddTestDevice("4402D075F5125BFF").Build();
+    }
+
+    #region 배너광고
+    const string bannerTestID = "ca-app-pub-3940256099942544/6300978111";
+    const string bannerID = "ca-app-pub-7297606231096997/2504193461";
+    BannerView bannerAd;
+
+    void LoadBannerAd() {
+        bannerAd = new BannerView(isTestMode ? bannerTestID : bannerID, AdSize.SmartBanner, AdPosition.Bottom);
+        bannerAd.LoadAd(GetAdRequest());
+    }
+    #endregion
+}
---
+++ Assassin/Assets/Script/CameraFixed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFixed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float fHeight = ((float)Screen.width / Screen.height) / ((float)16 / 9);
+        float fWeidth = 1f / fHeight;
+        if (fHeight < 1)
+        {
+            rect.height = fHeight;
+            rect.y = (1f - fHeight) / 2f;
+        }
+        else {
+            rect.width = fWeidth;
+            rect.x = (1f - fWeidth) / 2f;
+        }
+        camera.rect = rect;
+    }
+    //void OnPreCull() => GL.Clear(true, true, Color.black);
+}
